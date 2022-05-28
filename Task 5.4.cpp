9a2b35a09369f26bfcedc1bdec3cd202dfcb0527
dan222sydorak <106406948+dan222sydorak@users.noreply.github.com>
2022-05-28 13:51:24 +0300

    classwork 6

---
+++ Task 5.4.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+
+int fact(int n) {
+	int i, res;
+	i = 1;
+	res = 1;
+	while (i != n) {
+		i++;
+		res *= i;
+	}
+	return res;
+}
+
+double task_a5_4(int n) {
+	double res;
+	int i;
+	res = 1;
+	i = 1;
+	while (i != n) {
+		
+		res *= (1 + 1.0 / fact(i));
+		printf("%lf\n", res);
+		i++;
+		printf("%d\n", fact(i));
+	}
+	return res;
+}
+
+int main4() {
+	int x;
+	printf("n=");
+	scanf_s("%d", &x);
+	//printf("%d\n", fact(x));
+	printf("%lf\n", task_a5_4(x));
+	//printf("%d\n", fact(x));
+	return 0;
+}
