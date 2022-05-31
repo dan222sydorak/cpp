f1602c1d8c2860924727c8a55c2202c76c682d68
dan222sydorak <106406948+dan222sydorak@users.noreply.github.com>
2022-05-31 13:22:55 +0300

    classwork 4

---
+++ 4.1.cpp
@@ -0,0 +1,26 @@
+﻿// task 3.6
+
+#include <stdio.h>
+#include<iostream>
+
+	using namespace std;
+	int main() 
+	{
+		int a, b, c;
+		cout << " a=";
+		cin >> a;
+		cout << " b=";
+		cin >> b;
+		cout << " c=";
+		cin >> c;
+		if (a > b && a > c) {
+			cout << " the greatest:" << a;
+		}
+		else if (b > a && b > c) {
+			cout << " the greatest" << b;
+		}
+		else {
+			cout << " the  greatest" << c;
+		}
+		return 0;
+	}
---
+++ 4.11.cpp
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#include <float.h>
+#include <math.h>
+
+int main(int argc, char** argv) {
+	int i;
+	double x, sum = 0., sum_harm = 0.;
+	i = 0;
+	while (1) {
+		printf("a[%d]=", i);
+		scanf("&lf", &x);
+
+		if (fabs(x) <= 2 * DBL_EPSILON) break;
+		sum += x;
+		sum_harm += 1/x;
+		i++;
+	}
+	printf("avg=%lf, harm=%lf", sum / i, i / sum_harm);
+}
