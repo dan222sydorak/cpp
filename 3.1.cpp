58ee9ac315b5b3962998a1e2f2bcba6fb02104ba
dan222sydorak <106406948+dan222sydorak@users.noreply.github.com>
2022-05-31 13:21:30 +0300

    classwork 2

---
+++ 3.1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <cmath>
+
+float calculatePol(int x)
+{
+	int y;
+	y = pow(x, 5) + 5 * pow(x, 4) + 10 * pow(x, 2) + 5 * x + 1;
+
+	return(y);
+}
+
+void main()
+{
+	int x, l;
+	std::cout << "enter x:";
+	std::cin >> x;
+	l = calculatePol(x);
+	std::cout << l;
+}
---
+++ 3.2.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>  
+#include <cmath>
+using namespace std;
+
+void main()
+{
+    double x, y;
+    cin >> x;
+    cout << " p = " << pow(x, 5) + 5 * pow(x, 4) + 10 * pow(x, 2) + 5 * x + 1 << endl;
+}
