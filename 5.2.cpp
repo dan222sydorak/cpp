c52ce627263d640d42ffeb438b5e916eee9ece6a
dan222sydorak <106406948+dan222sydorak@users.noreply.github.com>
2022-05-31 13:24:27 +0300

    classwork 5

---
+++ 5.2.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+using namespace std;
+//a
+int main() {
+    int n, t1 = 0, t2 = 1, nextTerm = 0;
+
+    cout << "n: ";
+    cin >> n;
+
+    cout << "F=: ";
+
+    for (int i = 1; i <= n; ++i) {
+        if (i == 1) {
+            cout << t1 << ", ";
+            continue;
+        }
+        if (i == 2) {
+            cout << t2 << ", ";
+            continue;
+        }
+        nextTerm = t1 + t2;
+        t1 = t2;
+        t2 = nextTerm;
+
+        cout << nextTerm << ", ";
+    }
+    return 0;
+}
+
+//b
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t1 = 0, t2 = 1, nextTerm = 0, n;
+
+    cout << "n: ";
+    cin >> n;
+    cout << "F=: " << t1 << ", " << t2 << ", ";
+
+    nextTerm = t1 + t2;
+
+    while (nextTerm <= n) {
+        cout << nextTerm << ", ";
+        t1 = t2;
+        t2 = nextTerm;
+        nextTerm = t1 + t2;
+    }
+    return 0;
+}
+//г)
+#include <stdio.h>
+#include <iostream>
+
+int main() {
+    int last = 0, next = 1;
+    long sum = 1;
+
+    for (; next < 1000; next += last) {
+        last = next - last;
+        sum += next;
+    }
+    std::cout << sum;
+    return 0;
+}
---
+++ 5.5.cpp
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+size_t min_rec() {
+	size_t n = 3;
+	int x, x1, x2, x3;
+
+	x1 = x2 = x3 = -99;
+	while (x3 < 0) {
+		x = x1 + x3 + 100;
+		printf("%d=%d+%d+100\n", x, x1, x3);
+		x1 = x2;
+		x2 = x3;
+		x3 = x;
+		n++;
+	}
+	return n;
+}
+
+int main() {
+	printf("%zu", min_rec());
+}
