883b210cef33aa4d4a6ef93b5eec271dbd162202
dan222sydorak <106406948+dan222sydorak@users.noreply.github.com>
2022-05-31 12:03:41 +0300

    classwork 2

---
+++ 2.1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>                   
+#include <cmath>                      
+
+int main()
+{
+	double x, y;
+	std::cout << "x=";
+	std::cin >> x;
+	std::cout << "y=";
+	std::cin >> y;
+
+	double sum, product;
+	sum = x + y;
+	product = x * y;
+	std::cout << "summa=" << sum << std::endl;
+	std::cout << "product=" << product << std::endl;
+}
